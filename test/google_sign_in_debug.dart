8c2389629657e4d7acdeb2c261b201ef5ae31c01
Annabf7 <aborrasfont@gmail.com>
2026-02-17 11:15:36 +0100

    chore: cleanup debug file

--- test/google_sign_in_debug.dart
+++
@@ -1,6 +0,0 @@
-import 'package:google_sign_in/google_sign_in.dart';
-
-void main() {
-  final ensureConstructorExists = GoogleSignIn(scopes: ['email']);
-  print('GoogleSignIn instantiated: $ensureConstructorExists');
-}
